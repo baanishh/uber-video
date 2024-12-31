a35abf92cbdaa64891540a192e1698d40dc44e97
Banish A <110882343+baanishh@users.noreply.github.com>
2024-12-31 11:09:18 +0530

    error fixing done

--- frontend/src/pages/UserProtectedWrapper.jsx
+++ frontend/src/pages/UserProtectedWrapper.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { UserDataContext } from '../context/userContext'
+import { UserDataContext } from '../context/UserContext'
 
 const UserProtectedWrapper = ({children}) => {
 
